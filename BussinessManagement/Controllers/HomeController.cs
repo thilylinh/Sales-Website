0cad160aceba99fcf5e2bcd2c0cc2a6a337ac9e9
Linh <thilylinh98@gmail.com>
2020-02-06 15:35:45 +0700

    Add project files.

---
+++ BussinessManagement/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BussinessManagement.Models;
+
+namespace BussinessManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        BussinessEntities db = new BussinessEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult MenuPartial()
+        {
+            var lstProduct = db.SanPhams;
+            return PartialView(lstProduct);
+        }
+    }
+}
---
+++ BussinessManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BussinessManagement.MvcApplication" Language="C#" %>
