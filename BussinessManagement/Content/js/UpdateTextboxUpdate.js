a0626983c9a771e52f3010924dbad68a583323c7
Linh <thilylinh98@gmail.com>
2020-02-12 14:55:53 +0700

    do cart

---
+++ BussinessManagement/Content/js/UpdateTextboxUpdate.js
@@ -0,0 +1,7 @@
+﻿$("#btnUpdate").click(function () {
+    var Amount = $(".ChangeAmount").val();
+    if (isNaN(Amount) == true || Amount < 0) {
+        $("#showNotifi").text("The quantity is not valid");
+        return false;
+    }
+});
