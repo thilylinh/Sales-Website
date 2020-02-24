d4a11c1d459185306381b40886b138b407af55c0
Linh <thilylinh98@gmail.com>
2020-02-24 11:24:22 +0700

    do statistc, ordermanagement,receiver voucher

---
+++ BussinessManagement/Controllers/Admin/OrderManagementController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using BussinessManagement.Models;
+
+namespace BussinessManagement.Controllers.Admin
+{
+    public class OrderManagementController : Controller
+    {
+        BussinessEntities db = new BussinessEntities();
+        // new order and pay not yet
+        public ActionResult NewOrder()
+        {
+            var lstOrder = db.Orders.Where(n => n.IsPayed == false).OrderBy(n => n.OrderDate);
+            return View(lstOrder);
+        }
+        // delivery not yet
+        public ActionResult OrderDelivery()
+        {
+            var lstOrder  = db.Orders.Where(n => n.Status == false).OrderBy(n => n.OrderDate);
+            return View(lstOrder);
+        }
+        //payed and deliveried
+        public ActionResult OrderDone()
+        {
+            var lstOrder = db.Orders.Where(n => n.IsPayed == true && n.Status==true).OrderBy(n => n.OrderDate);
+            return View(lstOrder);
+        }
+        [HttpGet]
+        public ActionResult Approve(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order model = db.Orders.SingleOrDefault(n => n.IDOrder == id);
+            
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+         
+            var lstOrderDetail = db.TheOrderDetails.Where(n => n.ID == id);
+            ViewBag.ListOrderDetail = lstOrderDetail;
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Approve(Order ddh)
+        {
+            Order ddhUpdate = db.Orders.Single(n => n.IDOrder == ddh.IDOrder);
+            ddhUpdate.IsPayed = ddh.IsPayed;
+            ddhUpdate.Status = ddh.Status;
+            db.SaveChanges();
+            var lstOrderDetail = db.TheOrderDetails.Where(n => n.ID == ddh.IDOrder);
+            ViewBag.ListOrderDetail = lstOrderDetail;
+            return RedirectToAction("NewOrder");
+        }
+
+    }
+}
---
+++ BussinessManagement/Controllers/Admin/StatisticsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BussinessManagement.Models;
+
+namespace BussinessManagement.Controllers.Admin
+{
+    public class StatisticsController : Controller
+    {
+        BussinessEntities db = new BussinessEntities();
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            ViewBag.PageView = HttpContext.Application["PageView"];
+            ViewBag.StatisticOrderByMonth = StatisticOrderByMonth();
+            ViewBag.StatisticMoneyByMonth = StatisticMoneyByMonth();
+            return View();
+        }
+        public int StatisticOrderByMonth()
+        {
+            return db.Orders.Where(n => n.OrderDate.Value.Month == DateTime.Now.Month && n.OrderDate.Value.Year == DateTime.Now.Year).Count();
+            
+        }
+        public decimal StatisticMoneyByMonth()
+        {
+            var lstOrder = db.Orders.Where(n => n.OrderDate.Value.Month == DateTime.Now.Month && n.OrderDate.Value.Year == DateTime.Now.Year);
+            decimal total = 0;
+            foreach (var item in lstOrder)
+            {
+                total += item.TheOrderDetails.Sum(n => n.Amount * n.Price).Value;
+            }
+            return total;
+        }
+    }
+}
