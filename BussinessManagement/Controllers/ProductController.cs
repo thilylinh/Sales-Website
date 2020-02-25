70fb71a64be9ecf5373a48425b3c90222a067d5b
Linh <thilylinh98@gmail.com>
2020-02-25 17:17:58 +0700

    do paging for comment

--- BussinessManagement/Controllers/ProductController.cs
+++ BussinessManagement/Controllers/ProductController.cs
@@ -19,59 +19,6 @@ namespace BussinessManagement.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Product product = db.Products.Where(n => n.ID == id && n.IsDeleted == false).SingleOrDefault();
-            List<Comment> comment = db.Comments.Where(n=> n.IDProduct==id).ToList();
-            ViewBag.Comment = comment;
-            
-            //rating
-            double ratingTotal = 0;
-            int count = 0;
-            int r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0;
-            double ratioR1 = 0, ratioR2 = 0, ratioR3 = 0, ratioR4 = 0, ratioR5 = 0;
-            foreach (var i in comment)
-            {
-                if (i.Rate == 1)
-                {
-                    r1++;
-                }
-                if (i.Rate == 2)
-                {
-                    r2++;
-                }
-                if (i.Rate == 3)
-                {
-                    r3++;
-                }
-                if (i.Rate == 4)
-                { 
-                    r4++;
-                }
-                if (i.Rate == 5)
-                {
-                    r5++;
-                }
-                ratingTotal = (double)(ratingTotal + i.Rate);
-                count++;
-;            }
-            ratingTotal = ratingTotal / count;
-            ratioR1 = (double)((double)r1 / count) * 100;
-            ratioR2 = (double)((double)r2 / count) * 100;
-            ratioR3 = (double)((double)r3 / count) * 100;
-            ratioR4 = (double)((double)r4 / count) * 100;
-            ratioR5 = (double)((double)r5 / count) * 100;
-            //
-            ViewBag.R1 = r1;
-            ViewBag.R2 = r2;
-            ViewBag.R3 = r3;
-            ViewBag.R4 = r4;
-            ViewBag.R5 = r5;
-            //
-            ViewBag.RatioR1 = ratioR1;
-            ViewBag.RatioR2 = ratioR2;
-            ViewBag.RatioR3 = ratioR3;
-            ViewBag.RatioR4 = ratioR4;
-            ViewBag.RatioR5 = ratioR5;
-            ViewBag.RateTotal = ratingTotal;
-            //end rating
             if (product == null)
             {
                 return HttpNotFound();
@@ -134,6 +81,74 @@ namespace BussinessManagement.Controllers
             int pageCurrent = (page ?? 1);
             ViewBag.ProductTypeID = typeProductID;
             return View(lstProduct.OrderBy(n => n.Price).ToPagedList(pageCurrent, pageSize));
+        }
+        public PartialViewResult CommentForm(int? id,int ? page)
+        {
+            List<Comment> comment = db.Comments.Where(n => n.IDProduct == id).ToList();
+            ViewBag.Comment = comment;
+
+            //rating
+            double ratingTotal = 0;
+            int count = 0;
+            int r1 = 0, r2 = 0, r3 = 0, r4 = 0, r5 = 0;
+            double ratioR1 = 0, ratioR2 = 0, ratioR3 = 0, ratioR4 = 0, ratioR5 = 0;
+            foreach (var i in comment)
+            {
+                if (i.Rate == 1)
+                {
+                    r1++;
+                }
+                if (i.Rate == 2)
+                {
+                    r2++;
+                }
+                if (i.Rate == 3)
+                {
+                    r3++;
+                }
+                if (i.Rate == 4)
+                {
+                    r4++;
+                }
+                if (i.Rate == 5)
+                {
+                    r5++;
+                }
+                ratingTotal = (double)(ratingTotal + i.Rate);
+                count++;
+                ;
+            }
+            ratingTotal = ratingTotal / count;
+            ratioR1 = (double)((double)r1 / count) * 100;
+            ratioR2 = (double)((double)r2 / count) * 100;
+            ratioR3 = (double)((double)r3 / count) * 100;
+            ratioR4 = (double)((double)r4 / count) * 100;
+            ratioR5 = (double)((double)r5 / count) * 100;
+            //
+            ViewBag.R1 = r1;
+            ViewBag.R2 = r2;
+            ViewBag.R3 = r3;
+            ViewBag.R4 = r4;
+            ViewBag.R5 = r5;
+            //
+            ViewBag.RatioR1 = ratioR1;
+            ViewBag.RatioR2 = ratioR2;
+            ViewBag.RatioR3 = ratioR3;
+            ViewBag.RatioR4 = ratioR4;
+            ViewBag.RatioR5 = ratioR5;
+            if (ratingTotal != null)
+            {
+                ViewBag.RateTotal = ratingTotal;
+            }
+            else
+            {
+                ViewBag.RateTotal = 0;
+            }
+            //end rating
+            //paging 
+            int pageSize = 9;
+            int pageCurrent = (page ?? 1);
+            return PartialView(comment.ToPagedList(pageCurrent,pageSize));
         }
     }
 }
